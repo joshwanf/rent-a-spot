2533b33aae0def66109efd2331d76d112276ab82
Zachary <thez.haley@yahoo.com>
2024-09-18 13:46:20 -0400

    finished phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -10,5 +12,7 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
