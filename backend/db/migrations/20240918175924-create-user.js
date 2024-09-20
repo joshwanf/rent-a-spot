87cc448b55315af55706411492be4245a13e4863
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-09-20 13:35:01 -0400

    moved firstName lastName migration to original migration

--- backend/db/migrations/20240918175924-create-user.js
+++ backend/db/migrations/20240918175924-create-user.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -13,6 +14,14 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       username: {
         type: Sequelize.STRING(30),
