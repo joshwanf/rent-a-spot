8252f8d7991713959654e106371d0697ccd5b5f6
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-09-23 14:02:43 -0400

    Started /api/spots endpoints

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,7 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 router.post('/test', function(req, res) {
@@ -47,5 +48,7 @@ router.use('/users', usersRouter);
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
+
+router.use('/spots', spotsRouter);
 
 module.exports = router;
