d268ad78d444bc7f9061b7aa4978c55f97b319bb
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-10-10 23:54:18 -0400

    Completed front end auth me phase 0

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,26 +1,26 @@
 // backend/routes/api/index.js
-const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js');
-const reviewsRouter = require('./reviews.js');
-const bookingsRouter = require('./bookings.js');
-const spotImagesRouter = require('./spot-images.js');
-const reviewImagesRouter = require('./review-images.js');
+const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const spotsRouter = require("./spots.js");
+const reviewsRouter = require("./reviews.js");
+const bookingsRouter = require("./bookings.js");
+const spotImagesRouter = require("./spot-images.js");
+const reviewImagesRouter = require("./review-images.js");
 const { restoreUser } = require("../../utils/auth.js");
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-});
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+// });
 
 // GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+router.get("/set-token-cookie", async (_req, res) => {
   const user = await User.findOne({
     where: {
-      username: 'Demo-lition'
-    }
+      username: "Demo-lition",
+    },
   });
   setTokenCookie(res, user);
   return res.json({ user: user });
@@ -28,38 +28,31 @@ router.get('/set-token-cookie', async (_req, res) => {
 
 router.use(restoreUser);
 
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+router.get("/restore-user", (req, res) => {
+  return res.json(req.user);
+});
 
 // GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+const { requireAuth } = require("../../utils/auth.js");
+router.get("/require-auth", requireAuth, (req, res) => {
+  return res.json(req.user);
+});
 
-router.use('/session', sessionRouter);
+router.use("/session", sessionRouter);
 
-router.use('/users', usersRouter);
+router.use("/users", usersRouter);
 
-router.use('/reviews', reviewsRouter);
+router.use("/reviews", reviewsRouter);
 
-router.use('/bookings', bookingsRouter);
+router.use("/bookings", bookingsRouter);
 
-router.use('/spot-images', spotImagesRouter);
-router.use('/review-images', reviewImagesRouter);
+router.use("/spot-images", spotImagesRouter);
+router.use("/review-images", reviewImagesRouter);
 
-router.post('/test', (req, res) => {
+router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
 
-router.use('/spots', spotsRouter);
+router.use("/spots", spotsRouter);
 
 module.exports = router;
