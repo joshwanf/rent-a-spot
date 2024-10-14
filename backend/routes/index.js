31bc6ef10f9226e62f9911f0ab10d4e5b55ebc73
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-10-14 16:18:00 -0400

    figuring out __dirname

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,30 +11,16 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path");
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
-    console.log("__dirname:", __dirname);
-    console.log(
-      "sendFile:",
-      path.resolve(__dirname, "../../frontend", "dist", "index.html")
-    );
     res.cookie("XSRF-TOKEN", req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, "../../frontend", "dist", "index.html")
     );
   });
   // Serve the static assets in the frontend's build folder
-  console.log("express.static original:", path.resolve("../frontend/dist"));
-  console.log(
-    "express.static dirname:",
-    path.resolve(__dirname, "../../frontend/dist")
-  );
   router.use(express.static(path.resolve("../frontend/dist")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
-    console.log(
-      "not API route:",
-      path.resolve(__dirname, "../../frontend", "dist", "index.html")
-    );
     res.cookie("XSRF-TOKEN", req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, "../../frontend", "dist", "index.html")
