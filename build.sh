cb2b42dd29b0915eb4ad42ff8314e424098a389d
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-10-16 12:39:31 -0400

    added front end install and build scripts

--- build.sh
+++ build.sh
@@ -3,4 +3,11 @@
 cd frontend
     npm install
     npm run build
-    
+cd ..
+
+npm install 
+npm run build 
+npm run sequelize --prefix backend db:seed:undo:all 
+npm run sequelize --prefix backend db:migrate:undo:all 
+npm run sequelize --prefix backend db:migrate 
+npm run sequelize --prefix backend db:seed:all
