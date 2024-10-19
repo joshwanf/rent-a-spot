8a163ab82cc3b5e708f404f358b3336e711bb866
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-10-19 14:07:26 -0400

    checking the value of avgRating

--- frontend/src/store/spot.js
+++ frontend/src/store/spot.js
@@ -77,6 +77,7 @@ export const spotsReducer = (state = {}, action) => {
             ? `/spot-images/${spot.id}/${previewImage}`
             : "/no-image-found/no_image_available_600_x_450.svg";
         // convert avgRating to decimal with 1 sigfig
+        console.log("toFixed is not a function?", { avgRating });
         const rating = avgRating !== 0 ? avgRating.toFixed(1) : "New";
 
         newSpots[spot.id] = {
