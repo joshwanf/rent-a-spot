28107f850491db6758e7c51ac5722af48cae7d27
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-10-20 17:53:30 -0400

    added await to spot image seeder

--- backend/db/seeders/20240924045631-insert-spotimage.js
+++ backend/db/seeders/20240924045631-insert-spotimage.js
@@ -25,27 +25,7 @@ module.exports = {
       return images;
     });
     console.log("inserting images into spot", allImages);
-    SpotImage.bulkCreate(allImages);
-    // for (const spot of demoSpotImages) {
-    //   console.log("inserting images into spot", spot);
-    //   const images = spot.images.map((image) => ({
-    //     spotId: spot.spotId,
-    //     url: image.url,
-    //     preview: image.preview || false,
-    //   }));
-    //   SpotImage.bulkCreate(images);
-    //   // for (const image of images) {
-    //   // }
-    // }
-    // for (const spotImages of demoSpotImages) {
-    //   for (const image of spotImages.images) {
-    //     SpotImage.create({
-    //       spotId: spotImages.spotId,
-    //       url: image.url,
-    //       preview: image.preview || false,
-    //     });
-    //   }
-    // }
+    await SpotImage.bulkCreate(allImages);
   },
 
   async down(queryInterface, Sequelize) {
@@ -56,23 +36,12 @@ module.exports = {
      * await queryInterface.bulkDelete('People', null, {});
      */
     const spotIds = demoSpotImages.map((spot) => spot.spotId);
-    SpotImage.destroy({
+    await SpotImage.destroy({
       where: {
         spotId: {
           [Sequelize.Op.in]: spotIds,
         },
       },
     });
-    // for (const spotImages of demoSpotImages) {
-    //   for (const image of spotImages.images) {
-    //     SpotImage.destroy({
-    //       where: {
-    //         spotId: spotImages.spotId,
-    //         url: image.url,
-    //         preview: image.preview || false,
-    //       },
-    //     });
-    //   }
-    // }
   },
 };
